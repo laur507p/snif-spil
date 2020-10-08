ac008212b13cfec604051e3fd10b0ca0d70d768d
Laura Toft <laur507p@stud.kea.dk>
2020-10-08 12:47:32 +0200

    first commit in l branch

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// Show game screen
+// Snif explains the game
+// Game starts when start button is pressed
+// Bulbs start lighting up (randomly)
+// If a lit bulb is clicked, you get a point, status of bulb is set to on, sound and animation is played.
+// When the progress bar is full, you win
+// You receive the light medal
